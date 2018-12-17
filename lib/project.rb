6ea4757ca6f6bfe5aea2c234bef89ec32ec4bb50
Meisuleen Kased <meisuleen@gmail.com>
2018-12-17 04:45:49 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,8 +8,8 @@ class Project
   end
 
   def add_backer(backer)
-    @backed_projects << backer
-    backer.add_project(self)
+    # @backed_projects << backer
+    # backer.add_project(self)
   end
 
 end
